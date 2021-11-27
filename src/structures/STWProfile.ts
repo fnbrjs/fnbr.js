2ecf12378f6ee76a084d9fb4bb11d1aa88a24ea6
Vanxh <vanxhyt@gmail.com>
2021-11-27 17:25:52 +0530

    stw workers of profile

--- src/structures/STWProfile.ts
+++ src/structures/STWProfile.ts
@@ -1,6 +1,23 @@
 import Client from '../client/Client';
 import Base from '../client/Base';
-import { STWProfileData, STWProfileItemWithId, STWProfileStats } from '../../resources/httpResponses';
+import { CurveTable } from '../util/CurveTable';
+import HomebaseRatingMapping from '../../resources/STWMappings.json';
+import {
+  STWProfileData,
+  STWProfileItemWithId,
+  STWProfileStats,
+} from '../../resources/httpResponses';
+
+/**
+ * STW Power level curve table reader
+ */
+const powerLevelCurve = new CurveTable(
+  HomebaseRatingMapping[0].ExportValue.UIMonsterRating.Keys
+);
+
+interface KeyValuePair {
+  [key: string]: any;
+}
 
 /**
  * Represents a Save The World profile
@@ -57,6 +74,33 @@ class STWProfile extends Base {
    */
   public stats: STWProfileStats;
 
+  public powerLevel: number;
+
+  public leadSynergy: KeyValuePair = {
+    trainingteam: 'IsTrainer',
+    fireteamalpha: 'IsSoldier',
+    closeassaultsquad: 'IsMartialArtist',
+    thethinktank: 'IsInventor',
+    emtsquad: 'IsDoctor',
+    corpsofengineering: 'IsEngineer',
+    scoutingparty: 'IsExplorer',
+    gadgeteers: 'IsGadgeteer',
+  };
+
+  /**
+   * Survivor Squads for the Profile
+   */
+  public survivorSquads: KeyValuePair = {
+    trainingteam: [],
+    fireteamalpha: [],
+    closeassaultsquad: [],
+    thethinktank: [],
+    emtsquad: [],
+    corpsofengineering: [],
+    scoutingparty: [],
+    gadgeteers: [],
+  };
+
   /**
    * @param client The main client
    * @param data The profile data
@@ -73,8 +117,41 @@ class STWProfile extends Base {
     this.version = data.version;
     this.commandRevision = data.commandRevision;
 
-    this.items = Object.keys(data.items).map((k) => ({ id: k, ...data.items[k] }));
+    this.items = Object.keys(data.items).map((k) => ({
+      id: k,
+      ...data.items[k],
+    }));
     this.stats = data.stats.attributes;
+  }
+
+  /**
+   * Returns all survivors of the profile
+   */
+  public get workers(): Array<any> {
+    let arr = [];
+    for (const item of this.items) {
+      if (
+        item.templateId.startsWith('Worker:') &&
+        item.attributes.squad_slot_idx !== -1 &&
+        item.attributes.squad_id.length !== 0
+      ) {
+        arr.push(item);
+      }
+    }
+    return arr;
+  }
+
+  /**
+   * Update survivor squads
+   */
+  public updateSurvivorSquads(): KeyValuePair {
+    let survivors = this.workers;
+    for (let s of survivors) {
+      let squad = s.attributes.squad_id;
+      this.survivorSquads[squad.split('_')[3]].push(s);
+    }
+
+    return this.survivorSquads;
   }
 }
 

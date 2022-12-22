072ac0406152f23003c57f051fec2fc2af4a590c
Kye <promasterboy@outlook.com>
2022-12-22 14:11:08 +1100

    Added more platforms
    GFNMobile : Geforce Now Mobile
    HELIOSMOBILE : Xbox Cloud Gaming

--- resources/structs.ts
+++ resources/structs.ts
@@ -80,7 +80,7 @@ export type DeviceAuthResolveable = DeviceAuth | PathLike | DeviceAuthFunction |
 
 export type AuthStringResolveable = string | PathLike | StringFunction | StringFunctionAsync;
 
-export type Platform = 'WIN' | 'MAC' | 'PSN' | 'XBL' | 'SWT' | 'IOS' | 'AND' | 'PS5' | 'XSX';
+export type Platform = 'WIN' | 'MAC' | 'PSN' | 'XBL' | 'SWT' | 'IOS' | 'AND' | 'PS5' | 'XSX' | 'GFNMOBILE' | 'HELIOSMOBILE';
 
 export type AuthClient = 'fortnitePCGameClient' | 'fortniteIOSGameClient' | 'fortniteAndroidGameClient'
   | 'fortniteSwitchGameClient' | 'fortniteCNGameClient' | 'launcherAppClient2' | 'Diesel - Dauntless';
